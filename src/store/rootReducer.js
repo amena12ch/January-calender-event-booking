efbacea73d00f73325851ccc4b4bc340df57414c
amena12ch <41745728+amena12ch@users.noreply.github.com>
2021-05-03 01:54:58 -0700

    Update rootReducer.js

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,9 +2,8 @@ import { combineReducers } from 'redux';
 
 import CalenderReducer from '../reducers/Calender/CalenderReducer'
 import EventReducer from '../reducers/Event/EventReducer'
-import colorReducer from '../reducers/Color/colorReducer'
+
 export default combineReducers({
    calender: CalenderReducer,
-   event : EventReducer,
-   color : colorReducer
+   event : EventReducer
 });
